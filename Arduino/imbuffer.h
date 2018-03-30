ac58334225abde903feebe8ac1da92bed5078e10
Darek Mazur <darekm@madar.com.pl>
2018-03-30 20:39:24 +0200

    reformat:part III

--- Arduino/imbuffer.h
+++ Arduino/imbuffer.h
@@ -7,11 +7,8 @@
 #ifndef imBuffer_h
 #define imBuffer_h
 
-
 #include <imframe.h>
 //#.i.nclude <Arduino.h>
-
-
 
 typedef struct
 {
@@ -20,47 +17,45 @@ typedef struct
   uint16_t appended;
 } transfer_t;
 
-
-class  IMBuffer
+class IMBuffer
 {
-  private:
-      uint8_t tail;
-      uint8_t head;
-      uint8_t temp;
+private:
+  uint8_t tail;
+  uint8_t head;
+  uint8_t temp;
 
-    volatile byte ruptures[3];
-//    void setRssi();
+  volatile byte ruptures[3];
+  //    void setRssi();
 
-   public:
-    volatile  byte state;
-     byte _IM;
-    IMBuffer() {
-      _IM=99;
-    };
+public:
+  volatile byte state;
+  byte _IM;
+  IMBuffer()
+  {
+    _IM = 99;
+  };
 
-    transfer_t RX_buffer ;
-    transfer_t TX_buffer ;
-    unsigned short rSize;
-    unsigned short crc;
-    unsigned short rssiH;  //from last receive frame
-    void Init(byte channel);
-    bool Send();
-    void Read();
-    bool TestFrame();
-    uint8_t GetData();
-    bool Received();
-    void StartReceive();
-    void Sleep();
-    void Wakeup();
-    void Reboot();
-    bool Rupture();
-    void printReceive();
-    void printSend();
-    void setChannel(byte channel);
-    void setPower(byte power);
-    void setFunction(funTransceiver fun);
-
-} ;
-
+  transfer_t RX_buffer;
+  transfer_t TX_buffer;
+  unsigned short rSize;
+  unsigned short crc;
+  unsigned short rssiH; //from last receive frame
+  void Init(byte channel);
+  bool Send();
+  void Read();
+  bool TestFrame();
+  uint8_t GetData();
+  bool Received();
+  void StartReceive();
+  void Sleep();
+  void Wakeup();
+  void Reboot();
+  bool Rupture();
+  void printReceive();
+  void printSend();
+  void setChannel(byte channel);
+  void setPower(byte power);
+  void setFunction(funTransceiver fun);
+};
 
 #endif
--- Arduino/imrouting.h
+++ Arduino/imrouting.h
@@ -7,35 +7,32 @@
 #ifndef imRouting_h
 #define imRouting_h
 
-
 #include <Arduino.h>
 #include "imframe.h"
 #include "imdebug.h"
 
-
 #define MAXTableRouting 64
 class IMRouting
 {
-  private:
+private:
   byte count;
-  IMAddress WARD[MAXTableRouting];          //hop address
-  IMAddress ORIGIN[MAXTableRouting];        //device address
+  IMAddress WARD[MAXTableRouting];   //hop address
+  IMAddress ORIGIN[MAXTableRouting]; //device address
   IMMAC MACARRAY[MAXTableRouting];
-//  byte CHANNEL[MAXTableRouting];           //device channel listen
-  byte Send(IMFrame & frame);
-//  IMAddress Forward(IMAddress addr);
-  byte find(IMMAC  mac);
+  //  byte CHANNEL[MAXTableRouting];           //device channel listen
+  byte Send(IMFrame &frame);
+  //  IMAddress Forward(IMAddress addr);
+  byte find(IMMAC mac);
 
-  public:
-//  uint32_t time[MAXTableRouting];
- // IMAddress myId;
+public:
+  //  uint32_t time[MAXTableRouting];
+  // IMAddress myId;
   IMRouting();
   IMAddress Repeater(IMAddress addr);
-  bool addMAC(IMMAC mac,IMAddress ward);
-  byte addAddress(IMMAC mac,IMAddress addr, byte channel);
+  bool addMAC(IMMAC mac, IMAddress ward);
+  byte addAddress(IMMAC mac, IMAddress addr, byte channel);
   byte getChannel(IMAddress addr);
   void reset();
-
 };
 
 #endif
